f4aa03d78fbf7e8b584c4a09234bdb62ee34427d
Lucas Aschenbach <Lucas.Aschenbach@gmail.com>
2023-07-16 14:28:15 +0200

    Update KeyIcon style

--- src/components/KeyIcon.tsx
+++ src/components/KeyIcon.tsx
@@ -8,7 +8,7 @@ type Props = {
 const KeyIcon: React.FC<Props> = ({ className, children }) => {
   return (
     <div
-      className={`dark:text-white inline-block rounded border-b-2 border-zinc-700 bg-zinc-800 px-1 py-0.5 text-center text-xs font-semibold text-zinc-400 shadow-inner ${className}`}
+      className={`dark:text-white inline-block rounded border border-b-2 border-zinc-700 bg-zinc-900 px-1 py-0.5 text-center text-xs font-semibold text-zinc-400 shadow-inner ${className}`}
     >
       {children}
     </div>
